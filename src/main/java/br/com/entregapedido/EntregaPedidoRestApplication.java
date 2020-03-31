27a3d772f48920e0d71e15bb024015c5611db0f6
Mohammad Shadnik <mohammad.shadnik@gmail.com>
2020-03-31 11:24:31 -0300

    Criação do projeto

---
+++ src/main/java/br/com/entregapedido/EntregaPedidoRestApplication.java
@@ -0,0 +1,13 @@
+package br.com.entregapedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntregaPedidoRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntregaPedidoRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/entregapedido/EntregaPedidoRestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.entregapedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntregaPedidoRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
